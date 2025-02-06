e6bf4650b1ad5d2b3837d9d5a6d916687080c94c
douglytle <39708980+douglytle@users.noreply.github.com>
2025-02-05 19:36:28 -0500

    added launch file, updated setup.py to make the package actually work

---
+++ mail-delivery-robot/launch/robot.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+   return LaunchDescription([
+        Node(
+            package='sllidar_ros2',
+            executable='sllidar_node',
+            name='sllidar_node',
+            parameters=[{'channel_type': 'serial',
+                         'serial_port': '/dev/ttyUSB0',
+                         'serial_baudrate': 115200,
+                         'frame_id': 'laser',
+                         'inverted': False,
+                         'angle_compensate': True}],
+            output='screen'
+            ),
+        Node(
+            package='mail-delivery-robot',
+            namespace='sensors',
+            executable='camera_sensor',
+            name='camera_sensor'
+        )
+   ])
