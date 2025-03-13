c1aad48f2e52caa292efdf0cfbf97043bfec943c
douglytle <39708980+douglytle@users.noreply.github.com>
2025-03-13 19:55:54 -0400

    implemented intersection detection unit
    also changed some values in the config to make detection better

--- mail-delivery-robot/src/sensors/intersection_detection_unit.py
+++ mail-delivery-robot/src/sensors/intersection_detection_unit.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from std_msgs.msg import Bool
 
 class IntersectionDetectionUnit(Node):
     '''
@@ -21,36 +22,56 @@ class IntersectionDetectionUnit(Node):
         super().__init__('intersection_detection_unit')
 
         self.lidar_data_sub = self.create_subscription(String, 'lidar_data', self.lidar_data_callback, 10)
-        self.camera_data_sub = self.create_subscription(String, 'camera_data', self.camera_data_callback, 10)
+        self.camera_data_sub = self.create_subscription(Bool, 'camera_data', self.camera_data_callback, 10)
 
         self.intersection_detection_publisher = self.create_publisher(String, 'intersection_detection', 10)
-        self.intersection_detection_timer = self.create_timer(1, self.update_intersection_detection)
+        self.intersection_detection_timer = self.create_timer(0.5, self.update_intersection_detection)
 
         self.true_msg = String()
         self.true_msg.data = 'TRUE'
         self.false_msg = String()
         self.false_msg.data = 'FALSE'
+
+        self.lidar_indicates_intersection = False
+        self.camera_indicates_intersection = False
     
     def lidar_data_callback(self, data):
         '''
         The callback for /lidar_data.
         Reads information from the lidar sensor.
         '''
-        pass
+        lidar_data = str(data.data)
+
+        split_data = lidar_data.split(":")
+        
+        # waiting for the lidar to calibrate
+        if split_data[0] == "-1" and split_data[1] == "-1":
+            return
+        
+        if ((split_data[2] == "-1" and split_data[3] == "-1")
+            or (split_data[2] == "-1" and split_data[4] == "-1")
+            or (split_data[3] == "-1" and split_data[4] == "-1")):
+            self.lidar_indicates_intersection = True
+        else:
+            self.lidar_indicates_intersection = False
     
     def camera_data_callback(self, data):
         '''
         The callback for /camera_data.
         Reads information about intersection markers.
         '''
-        pass
+        #TODO: actually do something with the camera data.
+        self.camera_indicates_intersection = True
 
     def update_intersection_detection(self):
         '''
         The timer callback. Updates the internal state of this node and sends
         updates to /navigation when necessary
         '''
-        pass
+        if self.lidar_indicates_intersection and self.camera_indicates_intersection:
+            self.intersection_detection_publisher.publish(self.true_msg)
+        else:
+            self.intersection_detection_publisher.publish(self.false_msg)
 
 def main():
     rclpy.init()
