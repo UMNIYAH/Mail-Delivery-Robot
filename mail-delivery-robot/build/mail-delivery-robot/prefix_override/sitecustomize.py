c4f92510751102388b32f8bbb04477233ca6993c
Connor Yelle <yelleconnor@gmail.com>
2025-10-28 19:55:10 +0000

    Fully remove nested build/install/log

--- mail-delivery-robot/build/mail-delivery-robot/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/capstone2526/cmdr_ws/src/carleton-mail-delivery-robot/mail-delivery-robot/install/mail-delivery-robot'
